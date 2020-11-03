bbd4b4bb44098db28a26d3e9fa488e21b2591d4f
jasco-jasco <68643641+jasco-jasco@users.noreply.github.com>
2020-11-03 21:42:53 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  speed: 1000,
+  loop: true,
+
+  autoplay: {
+    delay: 4000
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
